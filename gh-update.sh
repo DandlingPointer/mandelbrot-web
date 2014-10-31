936e4d4d827808ff2298b218159492d3ab966e7d
Lennart Vogelsang <lennart@vogelsangs.net>
2014-10-31 01:45:01 +0100

    changes to script

--- gh-update.sh
+++ gh-update.sh
@@ -2,7 +2,15 @@ git reset --hard HEAD
 git checkout master
 cd ./tools
 node r.js -o build.js
+cp css/main.css out/main.css
+cp index.html out/index.html
 cd ..
 git checkout gh-pages
 rm -rf main.js
+rm -rf index.html
+rm -rf main.css
 cp out/main.js main.js
+cp out/index.html index.html
+cp out/main.css main.css
+sed -i "s//<script src=\"main.js\"></script>/g"
+sed -i "s/<link rel=\"stylesheet\" type=\"text\/css\" href=\"css\/main.css\">/<link rel=\"stylesheet\" type=\"text\/css\" href=\"main.css\">/g"
