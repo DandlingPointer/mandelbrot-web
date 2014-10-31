4ecf2e09736ba4db849cca49cf6569390d796971
Lennart Vogelsang <lennart@vogelsangs.net>
2014-10-31 02:01:47 +0100

    fix..

--- gh-update.sh
+++ gh-update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git reset --hard HEAD
 git checkout master
-node tools/r.js -o tools/build.js
+cd tools/
+node r.js -o build.js
 cp css/main.css out/main.css
 cp index.html out/index.html
 cd ..
